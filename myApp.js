934518c87412db308312073ae9b369b9281ba8c9
rizkietyap <setyakiki76@gmail.com>
2022-02-01 15:49:13 +0700

    send file as response

--- myApp.js
+++ myApp.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-console.log('Hello World');
+// console.log('Hello World');
 
 app.get('/', (req, res) => {
-  res.send('Hello Express');
+  const file_path = path.join(__dirname, 'views/index.html');
+  // console.log(file_path);
+  res.sendFile(file_path);
+  // res.send('Hello Express');
 });
 
 module.exports = app;
